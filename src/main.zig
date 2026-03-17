6daae298e0213102152a25b6bc60e853a2bfd3b4
salalaika <by965738071@163.com>
2026-03-17 18:28:13 +0800

    修改路径 也不行

--- src/main.zig
+++ src/main.zig
@@ -14,20 +14,16 @@ pub fn main(init: std.process.Init) !void {
 
     const url_raw = iter.next() orelse "test.mp4";
 
-    // Windows 路径需要转换为正斜杠，并添加 file:// 前缀
-    var buf: [512]u8 = undefined;
-    // 将反斜杠替换为正斜杠
+    // 将反斜杠转换为正斜杠（FFmpeg 需要）
     var path_buf: [512]u8 = undefined;
     var path_len: usize = 0;
     for (url_raw) |c| {
-        if (c == '\\') {
-            path_buf[path_len] = '/';
-        } else {
-            path_buf[path_len] = c;
-        }
+        path_buf[path_len] = if (c == '\\') '/' else c;
         path_len += 1;
     }
-    const url = try std.fmt.bufPrintZ(&buf, "file:///{s}", .{path_buf[0..path_len]});
+    
+    // 转换为以 null 结尾的字符串
+    const url = try std.fmt.bufPrintZ(&path_buf, "{s}", .{path_buf[0..path_len]});
 
     var app = try App.init(.{ .title = "media_player", .url = url, .width = 800, .height = 600, .is_resizable = true });
     try app.run();
